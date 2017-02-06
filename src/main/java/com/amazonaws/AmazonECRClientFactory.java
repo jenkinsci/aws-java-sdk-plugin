9f519ce1bd213176038359f1b3fd676f7183bc1f
Ivan Fernandez Calvo <kuisathaverat@yahoo.com>
2017-02-06 10:42:11 +0100

    minimise class

--- src/main/java/com/amazonaws/AmazonECRClientFactory.java
+++ src/main/java/com/amazonaws/AmazonECRClientFactory.java
@@ -2,7 +2,6 @@ package com.amazonaws;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.services.ecr.AmazonECRClient;
-import hudson.ProxyConfiguration;
 import jenkins.model.Jenkins;
 
 /**
@@ -11,44 +10,20 @@ import jenkins.model.Jenkins;
  * @author Marcus Wallin, kuisathaverat
  */
 public class AmazonECRClientFactory {
-    @edu.umd.cs.findbugs.annotations.SuppressFBWarnings(
-            value="NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE",
-            justification="The null value it is checked")
-    private final static class ClientConfigurationWrapper extends ClientConfiguration {
-        public ClientConfigurationWrapper() {
-            super();
-            Jenkins j = Jenkins.getInstance();
-            if (j != null && j.proxy != null) {
-                setProxyHost(j.proxy.name);
-                setProxyPort(j.proxy.port);
-                setProxyUsername(j.proxy.getUserName());
-                setProxyPassword(j.proxy.getPassword());
-            }
-        }
-
-    }
-
-    /**
-     * @return {@link AmazonECRClient} without proxy configuration.
-     */
-    public AmazonECRClient getAmazonECRClient() {
-        return new AmazonECRClient();
-    }
 
     /**
      * @param credentials {@link AWSCredentials} to use.
      * @return {@link AmazonECRClient} configured to use the proxy configured on Jenkins.
      */
     public AmazonECRClient getAmazonECRClientWithProxy(AWSCredentials credentials) {
-        return getAmazonECRClient(credentials, new ClientConfigurationWrapper());
-    }
-
-    /**
-     * @param credentials  {@link AWSCredentials} to use.
-     * @param clientConfig {@link ClientConfiguration} to create AmazonECRClient object.
-     * @return {@link AmazonECRClient} configured using credentials and clientConfig passed as parameters.
-     */
-    public AmazonECRClient getAmazonECRClient(AWSCredentials credentials, ClientConfiguration clientConfig) {
-        return new AmazonECRClient(credentials, clientConfig);
+        ClientConfiguration conf = new ClientConfiguration();
+        Jenkins j = Jenkins.getInstance();
+        if (j != null && j.proxy != null) {
+            conf.setProxyHost(j.proxy.name);
+            conf.setProxyPort(j.proxy.port);
+            conf.setProxyUsername(j.proxy.getUserName());
+            conf.setProxyPassword(j.proxy.getPassword());
+        }
+        return new AmazonECRClient(credentials, conf);
     }
 }
